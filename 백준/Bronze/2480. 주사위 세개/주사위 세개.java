5f4a810b3bad89dde6d3d0fdeb10c8d0b0e35187
Chaewon Park <109191101+muppychae1@users.noreply.github.com>
2024-01-28 18:09:37 +0900

    [Bronze IV] Title: 주사위 세개, Time: 120 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        br.close();
+
+        int prize = 0;
+        if(a == b && b == c && c == a) {
+            prize = 10000 + a * 1000;
+        }
+        else if((a == b && b != c) || (a == c && b != c)) {
+            prize = 1000 + a * 100;
+        }
+        else if(b == c && a != b) {
+            prize = 1000 + b * 100;
+        }
+        else {
+            int max = a;
+            if(max < b) { max = b; }
+            if(max < c) { max = c; }
+            prize = max * 100;
+        }
+
+        System.out.println(prize);
+    }
+}
