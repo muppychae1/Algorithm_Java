adb214a3795764caec5d589db0e9303ca5dd5bc8
Chaewon Park <109191101+muppychae1@users.noreply.github.com>
2024-01-30 21:45:38 +0900

    [Bronze II] Title: 단어의 개수, Time: 244 ms, Memory: 20056 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        br.close();
+
+        bw.write(Integer.toString(st.countTokens()));
+        bw.flush();
+        bw.close();
+    }
+}
